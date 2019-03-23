4bd5223ea305805c3b89bc6a62848a06b4d732ba
Machine <abderrahim223@gmail.com>
2019-03-23 11:19:41 -0400

    fix clentPrestige

--- tp4/source/ClientPrestige.h
+++ tp4/source/ClientPrestige.h
@@ -11,6 +11,7 @@
 class ClientPrestige : public ClientRegulier
 {
 public:
+	//Constructeur
 	ClientPrestige();
 	ClientPrestige(string_view nom, string_view prenom, int tailleGroupe, 
 		           int nbPoints, ZoneHabitation adresse);// TODO
@@ -25,7 +26,6 @@ public:
 
 private:
 	ZoneHabitation adresse_;
-
 };
 
 #endif
