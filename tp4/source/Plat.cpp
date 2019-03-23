dc978e4c960ccc4cb91390ad257c9d383326a093
Machine <abderrahim223@gmail.com>
2019-03-23 14:42:03 -0400

    ajout commentaire plat

--- tp4/source/Plat.cpp
+++ tp4/source/Plat.cpp
@@ -1,13 +1,17 @@
 /*
-* Date : 25 février 2019
-* Auteur : AbdeB
+INF1010 – PROGRAMMATION ORIENTEE OBJET
+Travail pratique No. 4
+file:    plat.h
+Auteur: ABDELKADER ZOBIRI 1891451 - Abderrahim Ammour 1924705
+Date: 25 mars 2019
+Description : Ce code contient les fonctions déclarées dans Plat.h
 */
 
 #include "Plat.h"
 #include "PlatBio.h"
 #include "PlatVege.h"
 #include "PlatBioVege.h"
-//#include "debogageMemoire.hpp"
+#include "debogageMemoire.hpp"
 using namespace std;
 
 Plat::Plat(string_view nom, double prix, double cout) : nom_(nom), prix_(prix), cout_(cout) {
@@ -42,16 +46,29 @@ bool Plat::operator < (const Plat& plat) const
 	return prix_ < plat.prix_;
 }
 
+/**********************************************************************************
+*Fonction : afficherPlat
+*Description : Methode permettant d'affiche les attributs du plat
+*Parametres : ostream& os
+*Retour : aucun
+***********************************************************************************/
 void Plat::afficherPlat(ostream & os) const
 { // TODO
 	os << "PLAT ----" << nom_ << " " << prix_ << "$" << "("<< cout_ <<"$ pour le restaurant)" << endl;
- 
+	
 }
 double Plat::getPrixRevient()
-{  return prix_ - cout_;
-    
+{  
+	return prix_ - cout_;    
 }
+
+/**********************************************************************************
+*Fonction : clone
+*Description : Methode permettant de de faire une copier l'objet 
+*Parametres : aucun
+*Retour : (Plat*) retourne un objet alloué dynamiquement 
+***********************************************************************************/
  Plat *  Plat:: clone () const
-{ //TODO
+{ 
 	 return new Plat(nom_, prix_, cout_);
 }
--- tp4/source/Plat.h
+++ tp4/source/Plat.h
@@ -1,6 +1,10 @@
 /*
-* Date : 25 février 2019
-* Auteur : AbdeB
+INF1010 – PROGRAMMATION ORIENTEE OBJET
+Travail pratique No. 4
+file:    plat.h
+Auteur: ABDELKADER ZOBIRI 1891451 - Abderrahim Ammour 1924705
+Date: 25 mars 2019
+Description : Ce code contient les déclarations des fonctions implémenté dans Plat.cpp
 */
 
 #ifndef PLAT_H
@@ -11,13 +15,14 @@
 #include <string_view>
 #include <iostream>
 #include "def.h"
-using std::string , std::string_view, std::ostream;  //? On ne devrait normalement pas faire de "using" global dans un .h, mais c'est accepte en INF1010.
+using std::string , std::string_view, std::ostream;  //On ne devrait normalement pas faire de "using" global dans un .h, mais c'est accepte en INF1010.
 
 class Plat {
 public:
 	// constructeur
-	Plat(string_view nom = "inconnu", double prix = 0, double cout = 0); // WTF is string_view
-         virtual ~Plat();
+	Plat(string_view nom = "inconnu", double prix = 0, double cout = 0); 
+	virtual ~Plat();
+
 	//getters 
 	string getNom() const;
 	double getPrix() const;
@@ -28,10 +33,10 @@ public:
 	void setPrix(double prix);
 
 	//methodes en plus
-        double getPrixRevient();
-        virtual Plat *  clone () const; // TODO
+	double getPrixRevient();
+	virtual Plat *  clone () const; // TODO
 	bool operator < (const Plat& plat) const;
-       virtual void afficherPlat(ostream& os) const; // TODO
+	virtual void afficherPlat(ostream& os) const; // TODO
 
 
 protected:
