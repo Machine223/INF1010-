0f54bb4cdc633adb66158f7ffec851c14caf15b8
Machine <abderrahim223@gmail.com>
2019-03-23 15:23:16 -0400

    ajout commentaire platVege

--- tp4/source/PlatVege.cpp
+++ tp4/source/PlatVege.cpp
@@ -1,23 +1,41 @@
 /*
-* Date : 25 février 2019
-* Auteur : AbdeB
+INF1010 – PROGRAMMATION ORIENTEE OBJET
+Travail pratique No. 4
+file:    platVege.cpp
+Auteur: ABDELKADER ZOBIRI 1891451 - Abderrahim Ammour 1924705
+Date: 25 mars 2019
+Description : Ce code contient les fonctions déclarées dans PlatVege.h
 */
+
 #include "PlatVege.h"
 
-
-//TODO
-PlatVege::PlatVege(string nom, double prix, double cout, double vitamines, double proteines, double mineraux): Plat(nom, prix, cout), Vege(nom,vitamines, proteines, mineraux)
+// Constructeur par parametres
+PlatVege::PlatVege(string nom, double prix, double cout, double vitamines, double proteines, double mineraux)
+	: Plat(nom, prix, cout), Vege(nom,vitamines, proteines, mineraux)
 {
 	setTaxe();
 }
+
+//Destructeur
 PlatVege::~ PlatVege(){}
 
+/**********************************************************************************
+*Fonction : PlatVege::clone
+*Description : Methode permettant de faire une copier l'objet
+*Parametres : aucun
+*Retour : (Plat*) retourne un objet alloué dynamiquement
+***********************************************************************************/
 Plat* PlatVege::clone()const
 { //TODO
 	return new PlatVege(Plat::nom_, prix_, cout_, vitamines_, proteines_, mineraux_);
 }
 
-
+/**********************************************************************************
+*Fonction : PlatVege::afficherPlat
+*Description : Methode permettant d'affiche les attributs du plat vegetarien
+*Parametres : ostream& os
+*Retour : aucun
+***********************************************************************************/
 void PlatVege::afficherPlat(ostream & os) const 
 {   //TODO
 	Plat::afficherPlat(os);
@@ -25,16 +43,24 @@ void PlatVege::afficherPlat(ostream & os) const
 	os << "(Apport nutritif:" << calculerApportNutritif() << " mg)" << endl;
 }
 
+/**********************************************************************************
+*Fonction : PlatVege::calculerApportNutritif
+*Description : Methode permettant de calculer l'apport nutritif d'un plat vegetarien
+*Parametres : aucun
+*Retour : double
+***********************************************************************************/
 double PlatVege::calculerApportNutritif() const
 {
 	return (vitamines_ * proteines_ / mineraux_)*RAPPORT_NUTRITIF;
 }
 
+// Le pourcentage de la taxe d'un plat vegetarien est de 0.07
 void PlatVege::setTaxe()
 {
 	taxe_ = 0.07;
 }
 
+// Methode d'acces à l'attribut taxe_
 double PlatVege::getTaxe() const
 {
 	return taxe_;
--- tp4/source/PlatVege.h
+++ tp4/source/PlatVege.h
@@ -1,25 +1,36 @@
 /*
-* Date : 25 février 2019
-* Auteur : AbdeB
+INF1010 – PROGRAMMATION ORIENTEE OBJET
+Travail pratique No. 4
+file:    platVege.h
+Auteur: ABDELKADER ZOBIRI 1891451 - Abderrahim Ammour 1924705
+Date: 25 mars 2019
+Description : Ce code contient les déclarations des fonctions implémenté dans PlatVege.cpp
 */
+
 #ifndef PLAT_VEGE_H
 #define PLAT_VEGE_H
 
 #include "Vege.h"
 #include "Plat.h"
 #include "def.h"
-class PlatVege :
-        public Vege, public Plat, public Taxable
+class PlatVege :public Vege, public Plat, public Taxable
 {
 public:
+	//Constructeur
 	PlatVege(string nom = "inconnu", double prix = 0, double cout = 0, double vitamines = 0,
-             double proteines = 0, double mineraux = 0);//TODO
-     virtual ~ PlatVege();
-     Plat * clone()const; // TODO
-     void afficherPlat(ostream & os) const;//TODO
-     double calculerApportNutritif() const; // TODO
-	 void setTaxe() override; // Avoir
-	 double getTaxe()const override; // A voir
+	double proteines = 0, double mineraux = 0);//TODO
+
+	//Destructeur
+	virtual ~ PlatVege();
+
+	//Autres methode
+	Plat * clone()const; // TODO
+	void afficherPlat(ostream & os) const;//TODO
+	double calculerApportNutritif() const; // TODO
+
+	// Heritage de Vege.h et sont modifie
+	void setTaxe() override; // Avoir
+	double getTaxe()const override; // A voir
 protected:
 	// Ajout de l'attribut taxe_
 	double taxe_;
