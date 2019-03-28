46e9ef3331cba1db51d4c03dc4266c1857121222
Machine <abderrahim223@gmail.com>
2019-03-28 15:52:33 -0400

    ajout dossier tp5

--- tp4/source/Boisson.h
+++ tp4/source/Boisson.h
@@ -10,11 +10,14 @@
 #include "Taxable.h"
 #include <string>
 #include <string_view>
+
 using namespace std;
 class Boisson : public Taxable // TODO
 {
  public:
     Boisson (string_view nom, double prix);
+	virtual ~Boisson() {};
+
     string_view getNom() const;
     double getPrix () const;
 	void setTaxe() override; // Avoir
